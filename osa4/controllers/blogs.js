e0c196224aa60ae6f9da54e325373b2e3f7aae7b
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-04-03 14:13:47 +0300

    4.14 done

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -41,4 +41,30 @@ blogsRouter.post('/', async (request, response) => {
   // })
 })
 
+blogsRouter.delete('/:id' ,async ( request, response) => {
+  const id = request.params.id
+const deleted = await Blog.findByIdAndRemove(id)
+response.status(204).end()
+})
+
+
+
+blogsRouter.put('/:id', async (request, response,) => {
+const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.author,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog,  { new: true })
+  response.json(updatedBlog)
+})
+
+
+
+
+
 module.exports = blogsRouter
