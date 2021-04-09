6a6fe343ae5e6ca0eb7fc4545714b66cf6d1f3a6
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-04-09 22:37:56 +0300

    4.20

--- osa4/app.js
+++ osa4/app.js
@@ -5,11 +5,15 @@ const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const middleware = require ('./utils/middleware')
+
 
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(express.json())
+
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 
--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -14,23 +14,26 @@ blogsRouter.get('/', async (request, response) => {
 
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
 
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  // const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
+  
   const user = await User.findById(decodedToken.id)
 
 
@@ -61,10 +64,6 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog)
   }
 
-
-  // .then(result => {
-  //   response.status(201).json(result)
-  // })
 })
 
 blogsRouter.delete('/:id' ,async ( request, response) => {
@@ -89,6 +88,14 @@ const body = request.body
   response.json(updatedBlog)
 })
 
+
+// const tokenExtractor = (request, response ,next) => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     request.token = authorization.substring(7)
+//   }
+//   return null
+// }
 
 
 
